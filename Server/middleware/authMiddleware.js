b5a807c13a26f75c2b3935e90ccfe440841782fd
renx19 <138985568+renx19@users.noreply.github.com>
2026-01-17 18:22:14 +0800

    Update authMiddleware.js

--- Server/middleware/authMiddleware.js
+++ Server/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 // middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../Models/user');
 
 const authenticateUser = async (req, res, next) => {
   try {
