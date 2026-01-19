494d7cf1b163a85daf162b1a9c8126d6198ee4e9
renx19 <138985568+renx19@users.noreply.github.com>
2026-01-19 20:44:58 +0800

    z

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const userController = require('../controllers/AuthController');
 const authenticateUser = require('../middleware/authMiddleware');
 
-router.post('/login', userController.loginUser);
+
 router.post('/logout', userController.logoutUser);
 router.post('/refresh', userController.refreshToken);
 router.post('/create', userController.createUser);
-
+router.post('/login', userController.loginUser);
 // Protected route example
 router.get('/profile', authenticateUser, (req, res) => {
   res.json({ message: 'User is authenticated!', user: req.user });
